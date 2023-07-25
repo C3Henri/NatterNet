1dc711684ec88de6b7475344f2a95faffb235646
Henrique <cesarhenrique676@gmail.com>
2023-07-24 22:14:36 -0300

    feature/ui-color-and-settings-update

--- natternet/src/components/SettingsPage.js
+++ natternet/src/components/SettingsPage.js
@@ -1,86 +1,110 @@
-import React, { useState } from 'react';
-import { FaArrowLeft } from 'react-icons/fa';
+/* SettingsPage.js */
+
+import React from 'react';
+import { Layout, Menu, Form, Input, Button, Select, Typography } from 'antd';
+import { UserOutlined, MessageOutlined, ArrowLeftOutlined, MenuFoldOutlined, MenuUnfoldOutlined } from '@ant-design/icons';
 import { useNavigate } from 'react-router-dom';
 import './SettingsPage.css';
 
+const { Sider, Content } = Layout;
+const { Option } = Select;
+const { Title } = Typography;
+
 function SettingsPage() {
+    const [selectedKey, setSelectedKey] = React.useState("profile");
+    const [collapsed, setCollapsed] = React.useState(false);
+    const [backVisible, setBackVisible] = React.useState(true);
     const navigate = useNavigate();
-    const [selectedOption, setSelectedOption] = useState(null);
+
+    const handleMenuClick = (e) => {
+        setSelectedKey(e.key);
+    }
 
     const handleBackClick = () => {
-        navigate('/home');
-    };
+        navigate(-1);
+    }
 
-    const handleOptionClick = (option) => {
-        setSelectedOption(option);
-    };
-
-    const renderOptionContent = () => {
-        switch(selectedOption) {
-            case 'profile':
-                return (
-                    <div className="contentForm">
-                        <div className="inputGroup">
-                            <label>Nome:</label>
-                            <input type="text" className="inputField" />
-                        </div>
-                        <div className="inputGroup">
-                            <label>Email:</label>
-                            <input type="email" className="inputField" />
-                        </div>
-                        <div className="inputGroup">
-                            <label>Senha:</label>
-                            <input type="password" className="inputField" />
-                        </div>
-                    </div>
-                );
-            case 'chat':
-                return (
-                    <div className="contentForm">
-                        <div className="inputGroup">
-                            <label>Método de salvar:</label>
-                            <select className="inputField">
-                                <option>Servidor</option>
-                                <option>Peer to peer</option>
-                            </select>
-                        </div>
-                    </div>
-                );
-            default:
-                return (
-                    <div className="introMessage">
-                        Selecione uma opção ao lado e personalize como desejar para melhorar ainda mais sua experiência :)
-                    </div>
-                );
-        }
+    const toggle = () => {
+        setCollapsed(!collapsed);
     };
 
     return (
-        <div className="settingsPage">
-            <div className="settingsHeader">
-                <button className="backButton" onClick={handleBackClick}>
-                    <FaArrowLeft className="backIcon" />
-                </button>
-                <h2>Configurações</h2>
-            </div>
-            <div className="settingsBody">
-                <div className="settingsOptions">
-                    <button 
-                        className={`settingsItem ${selectedOption === 'profile' ? 'selected' : ''}`} 
-                        onClick={() => handleOptionClick('profile')}>
+        <Layout className="settingsPage">
+            <Sider width={collapsed ? 80 : "25%"} className="settingsSider" collapsed={collapsed} onCollapse={(collapsed) => setBackVisible(!collapsed)}>
+    <div className="siderHeader">
+        {backVisible ? (
+            <ArrowLeftOutlined onClick={handleBackClick} className="backIcon" />
+        ) : (
+            <div className="backIconPlaceholder" />
+        )}
+        {!collapsed && <Title level={3} style={{ color: '#fff', padding: '20px 0'}}>Configurações</Title>}
+        {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+            className: 'trigger',
+            onClick: toggle,
+        })}
+    </div>
+                <Menu
+                    onClick={handleMenuClick}
+                    selectedKeys={[selectedKey]}
+                    mode="inline"
+                >
+                    <Menu.Item key="profile" icon={<UserOutlined />}>
                         Meu Perfil
-                    </button>
-                    <button 
-                        className={`settingsItem ${selectedOption === 'chat' ? 'selected' : ''}`} 
-                        onClick={() => handleOptionClick('chat')}>
+                    </Menu.Item>
+                    <Menu.Item key="chat" icon={<MessageOutlined />}>
                         Personalização de chat
-                    </button>
-                </div>
-                <div className="settingsContent">
-                    {renderOptionContent()}
-                </div>
-            </div>
-        </div>
+                    </Menu.Item>
+                </Menu>
+            </Sider>
+            <Content className="settingsContent">
+                {selectedKey === "profile" && (
+                    <Form className="settingsForm">
+                        <Form.Item
+                            name="name"
+                            rules={[{ required: true, message: 'Por favor insira seu nome!' }]}
+                        >
+                            <Input placeholder="Nome" />
+                        </Form.Item>
+                        <Form.Item
+                            name="email"
+                            rules={[{ required: true, message: 'Por favor insira seu email!' }]}
+                        >
+                            <Input placeholder="Email" />
+                        </Form.Item>
+                        <Form.Item
+                            name="password"
+                            rules={[{ required: true, message: 'Por favor insira sua senha!' }]}
+                        >
+                            <Input.Password placeholder="Senha" />
+                        </Form.Item>
+                        <Form.Item>
+                            <Button type="primary" htmlType="submit">
+                                Salvar
+                            </Button>
+                        </Form.Item>
+                        </Form>
+                )}
+
+                {selectedKey === "chat" && (
+                    <Form className="settingsForm">
+                        <Form.Item
+                            name="saveMethod"
+                            rules={[{ required: true, message: 'Por favor selecione um método!' }]}
+                        >
+                            <Select placeholder="Selecione um método de salvamento">
+                                <Option value="server">Servidor</Option>
+                                <Option value="peer">Peer to peer</Option>
+                            </Select>
+                        </Form.Item>
+                        <Form.Item>
+                            <Button type="primary" htmlType="submit">
+                                Salvar
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                )}
+            </Content>
+        </Layout>
     );
 }
 
